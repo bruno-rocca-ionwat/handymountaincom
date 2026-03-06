ddf076fd30c9710f0ba49a1b4b6b63d3ef45ac1e
ionwat <bruno@ionwat.fr>
2026-03-05 17:16:38 -0800

    Change heading text to 'Your Unique Quote Reference'

--- src/pages/blobs/_components/NewShape.tsx
+++ src/pages/blobs/_components/NewShape.tsx
@@ -40,7 +40,7 @@ export default function NewShape(props: Props) {
 
     return (
         <>
-            <h2 className="mb-4 text-xl text-center sm:text-xl">New Random Shape</h2>
+            <h2 className="mb-4 text-xl text-center sm:text-xl">Your Unique Quote Reference</h2>
             <div className="w-full mb-6 bg-white rounded-lg">
                 <div className="p-4 text-center text-gray-900 border-b border-gray-200 min-h-14">{blobData && <span>{blobData.parameters?.name}</span>}</div>
                 <div className="p-4 aspect-square text-primary">{blobData && <ShapePreview {...blobData} />}</div>
